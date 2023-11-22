8f7fc40ff1d635b2190380f56ebc3d1aa1ec85d4
SwykanySolutions <swykanysolutions@gmail.com>
2023-11-22 12:52:32 -0300

    My first commit

---
+++ components/Head/index.php
@@ -0,0 +1,8 @@
+<head>
+    <script src="/StockControl/js/color-modes.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StockControl</title>
+    <link rel="stylesheet" href="/StockControl/css/global.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php require_once("./components/Head/index.php"); ?>
+<body>
+    <?php require_once("./components/Body/index.php"); ?>
+</body>
+</html>
